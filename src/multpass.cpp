8d207d04848ec6530306cf0ef70a196f0613c328
geng_bl@126.com <geng_bl@126.com>
2022-04-10 15:50:23 +0800

    增加透明显示技术示例

--- src/multpass.cpp
+++ src/multpass.cpp
@@ -2,32 +2,93 @@
 #include <stdlib.h>
 #include <iostream>
 #include <osgDB/ReadFile>
-#include <osgFX/Cartoon>
+#include <osgFX/Technique>
+#include <osgFX/Effect>
+#include <osg/Material>
+#include <osg/Depth>
+#include <osg/BlendFunc>
 #include "app.h"
+
+class TransparencyTechnique : public osgFX::Technique
+{
+public:
+    TransparencyTechnique() : osgFX::Technique() {}
+    virtual bool validate( osg::State& ss ) const
+        {
+            return true;
+        }
+protected:
+    virtual void define_passes()
+        {
+            osg::ref_ptr<osg::StateSet> ss = new osg::StateSet;
+            ss->setAttributeAndModes( new osg::ColorMask(
+                                          false, false, false, false) );
+            ss->setAttributeAndModes( new osg::Depth(osg::Depth::LESS) );
+            addPass( ss.get() );
+            ss = new osg::StateSet;
+            ss->setAttributeAndModes( new osg::ColorMask(
+                                          true, true, true, true) );
+            ss->setAttributeAndModes( new osg::Depth(osg::Depth::EQUAL) );
+            addPass( ss.get() );
+        }
+};
+
+
+class TransparentcyEffect : public osgFX::Effect
+{
+public:
+    TransparentcyEffect() : osgFX::Effect() {}
+    TransparentcyEffect( const TransparentcyEffect& copy,
+                      const osg::CopyOp op=osg::CopyOp::SHALLOW_COPY )
+        : osgFX::Effect(copy, op) {}
+    META_Effect( osgFX, TransparentcyEffect, "TransparentcyEffect",
+                 "", "" );
+protected:
+    virtual bool define_techniques()
+        {
+            addTechnique(new TransparencyTechnique);
+            return true;
+        }
+};
+
 
 class MultpassApp : public Common::App
 {
 protected:
     ~MultpassApp(){
-        
+
     }
 public:
     virtual void init( int argc, char *argv[] )
         {
-            osgFX::Cartoon *lpCartoon = new osgFX::Cartoon;
+            osg::Group *lpGroup = new osg::Group;
             if ( argc >= 2 )
             {
                 for ( int i = 1; i < argc; i ++ )
                 {
-                    lpCartoon->addChild( osgDB::readNodeFile(argv[i]) );
+                    if ( i == 1 )
+                    {
+                        lpGroup->addChild( osgDB::readNodeFile(argv[i]) );
+                    }else
+                    {
+                        _root->addChild(osgDB::readNodeFile(argv[i]));
+                    }
                 }
             }else
             {
-                lpCartoon->addChild( osgDB::readNodeFile("cow.osg") );
+                lpGroup->addChild( osgDB::readNodeFile("cow.osg") );
             }
 
-            _root->addChild( lpCartoon );
-            //_viewer->setSceneData( lpCartoon );
+            TransparentcyEffect *lpEffect = new TransparentcyEffect;
+            //osg::Group *lpEffect = new osg::Group;
+            lpEffect->addChild( lpGroup );
+            _root->addChild( lpEffect );
+            osg::ref_ptr<osg::Material> material = new osg::Material;
+            material->setAmbient( osg::Material::FRONT_AND_BACK, osg::Vec4(0.0f, 0.0f, 0.0f, 0.3f) );
+            material->setDiffuse( osg::Material::FRONT_AND_BACK, osg::Vec4(1.0f, 1.0f, 1.0f, 0.3f) );
+            lpGroup->getOrCreateStateSet()->setAttributeAndModes(material.get(), osg::StateAttribute::ON|osg::StateAttribute::OVERRIDE );
+            lpGroup->getOrCreateStateSet()->setAttributeAndModes(new osg::BlendFunc );
+            lpGroup->getOrCreateStateSet()->setRenderingHint(osg::StateSet::TRANSPARENT_BIN );
         }
 
 };
